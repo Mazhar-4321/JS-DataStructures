99a9a9565fbbbda0417c54be40c474fffaf62d69
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-15 21:47:07 +0530

    ADDED: Code To Find Repeated Twice Digits In 0-100 Range

--- array/Main.js
+++ array/Main.js
@@ -7,3 +7,8 @@ console.log(array.reduce((v1,v2)=>v1<=v2?v1:v2))
 array.push(Math.floor(Math.random()*10))
 console.log((array.sort())[1])
 console.log((array.sort().reverse())[1])
+let array1=[]
+for(let i=11;i<=100;i+=11){
+    array1.push(i)
+}
+console.log(array1)
