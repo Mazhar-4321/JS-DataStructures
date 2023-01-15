a7029e8c8274c96b7bccb962550c62ffcbdcad42
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-15 22:05:59 +0530

    ADDED: Code To Store Similiar Birth Months Of 50 Persons In Map

--- map/Main.js
+++ map/Main.js
@@ -1,24 +1,37 @@
 let numbersMap = new Map();
-let minValue=10;
-let maxValue=10;
-while(true){
-    let number = Math.floor(Math.random()*10)%6+1;
-    if(numbersMap.get(number)){
-        let value=numbersMap.get(number)
-        numbersMap.set(number,value+1);
-        if(value+1==10){
-            maxValue=number;
+let minValue = 10;
+let maxValue = 10;
+while (true) {
+    let number = Math.floor(Math.random() * 10) % 6 + 1;
+    if (numbersMap.get(number)) {
+        let value = numbersMap.get(number)
+        numbersMap.set(number, value + 1);
+        if (value + 1 == 10) {
+            maxValue = number;
             break;
         }
-    }else{
-        numbersMap.set(number,1);
+    } else {
+        numbersMap.set(number, 1);
     }
 }
-let minNumber=0;
-for(const[key,value] of numbersMap.entries()){
-        if(minValue>value){
-            minValue=value;
-            minNumber=key
-        }
+let minNumber = 0;
+for (const [key, value] of numbersMap.entries()) {
+    if (minValue > value) {
+        minValue = value;
+        minNumber = key
+    }
 }
-console.log(numbersMap, maxValue,minNumber)
+console.log(numbersMap, maxValue, minNumber)
+
+let count = 50;
+let birthMap= new Map();
+while (count > 0) {
+    count -= 1;
+   let month = Math.floor(Math.random()*10)%12+1;
+   if(birthMap.get(month)){
+     birthMap.set(month,birthMap.get(month)+1)
+   }else{
+    birthMap.set(month,1);
+   }
+}
+console.log(birthMap)
