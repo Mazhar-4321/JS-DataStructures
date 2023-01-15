5afc43eb89a9586d46db8453afe32c95b60a4546
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-15 21:44:06 +0530

    ADDED: Code To Sort Array And Find Second Large And Small

--- array/Main.js
+++ array/Main.js
@@ -4,3 +4,6 @@ array.push(Math.floor(Math.random()*10))
 array.push(Math.floor(Math.random()*10))
 let initialValue=array[0]
 console.log(array.reduce((v1,v2)=>v1<=v2?v1:v2))
+array.push(Math.floor(Math.random()*10))
+console.log((array.sort())[1])
+console.log((array.sort().reverse())[1])
